e096eefb22dd5091faea941555aeb3e834bb6b36
Awsty <lianga@oregonstate.edu>
2019-03-19 20:47:54 -0700

    Uploading progress
    ButtonHandlers1 is the only thing that is runnable, everything else has errors and cannot run at the moment.

---
+++ lianga/ButtonHandler1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonFunction : MonoBehaviour
+{
+    // Update is called once per frame
+    void SetText(string text)
+    {
+        Text txt = transform.Find("Text").GetComponent<Text>();
+        txt.text = text;
+    }
+}
---
+++ lianga/Rotation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            //transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            //transform.Rotate(Vector3.down * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            //transform.Rotate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            //transform.Rotate(Vector3.left * speed * Time.deltaTime);
+        }
+
+    }
+}
