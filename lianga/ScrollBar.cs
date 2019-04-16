35832cd1e6a9982c4e235ba59f80543ee69f56e7
Awsty <lianga@oregonstate.edu>
2019-04-15 23:15:43 -0700

    Update on Scroll Bar. Backup compiles
    I have not tested the backup, but it compiles currently. I have implemented the dynamic targeting for 3d objects but am unsure if it will work properly. I do not have an android device to test with.

---
+++ lianga/ScrollBar.cs
@@ -0,0 +1,67 @@
+﻿//Testing this code to use, resource: https://developer.vuforia.com/forum/faq/unity-how-can-i-dynamically-swap-3d-model-target
+using UnityEngine;
+using System.Collections;
+
+public class ModelSwapper : MonoBehaviour
+{
+
+    public Vuforia.TrackableBehaviour theTrackable;
+
+    private bool mSwapModel = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (theTrackable == null)
+        {
+            Debug.Log("Warning: Trackable not set !!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mSwapModel && theTrackable != null)
+        {
+            SwapModel();
+            mSwapModel = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(50, 50, 120, 40), "Swap Model"))
+        {
+            mSwapModel = true;
+        }
+    }
+
+    private void SwapModel()
+    {
+
+        GameObject trackableGameObject = theTrackable.gameObject;
+
+        //disable any pre-existing augmentation
+        for (int i = 0; i < trackableGameObject.transform.GetChildCount(); i++)
+        {
+            Transform child = trackableGameObject.transform.GetChild(i);
+            child.gameObject.active = false;
+        }
+
+        // Create a simple cube object 
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        // Re-parent the cube as child of the trackable gameObject
+        cube.transform.parent = theTrackable.transform;
+
+        // Adjust the position and scale 
+        // so that it fits nicely on the target
+        cube.transform.localPosition = new Vector3(0, 0.2f, 0);
+        cube.transform.localRotation = Quaternion.identity;
+        cube.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+        // Make sure it is active
+        cube.active = true;
+    }
+}
