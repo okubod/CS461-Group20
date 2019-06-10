cc9f40f88df5e4af014fdc6fadaf54358df8542d
Christopher Cooper <christopher.cooper978@gmail.com>
2019-06-10 07:06:22 -0700

    Update connectvars.php
    Removed Autodesk account info

--- maintenanceWebApp/connectvars.php
+++ maintenanceWebApp/connectvars.php
@@ -1,7 +1,7 @@
 <?php
    // Define autodesk connection constants
-   define('ID', 'YrRNlAgMCSW8gqFzo2fTw0GRzHYEUjiy');
-   define('URI', 'http%3A%2F%2Fweb.engr.oregonstate.edu%2F~coopchri%2Finfrastructure_maintenance%2Fautodesk_redirect.php');
-   define('SECRET', 'XwXsq7V71JnBxK7U');
+   define('ID', '');
+   define('URI', '');
+   define('SECRET', '');
 ?>
 
